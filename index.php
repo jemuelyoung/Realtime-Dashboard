7b40ee27eb4507e13236eac01e7d2259b17297cd
Jemuel Young <jemuel.young@gmail.com>
2012-08-13 05:28:50 +0000

    changes to index

--- index.php
+++ index.php
@@ -2,37 +2,12 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<script src="./includes/jquery.min.js"></script>
-		<script src="./includes/d3.v2.min.js"></script>
+				<script src="./includes/jquery.min.js"></script>
 		<script src="http://23.23.183.118:4000/socket.io/socket.io.js"></script>
 		<script src="client.js"></script>
-		<script type='text/javascript' src='https://www.google.com/jsapi'></script>
-		<script type="text/javascript" src="./includes/smoothie.js"></script>
-		<script type='text/javascript'>
-			google.load('visualization', '1', {
-				packages : ['gauge']
-			});
-			google.setOnLoadCallback(drawChart);
-			function drawChart() {
-				var data1 = google.visualization.arrayToDataTable([['Label', 'Value'], ['CPU', 15]]);
-				var data2 = google.visualization.arrayToDataTable([['Label', 'Value'], ['Network', 68]]);
-
-				var options = {
-					width : 800,
-					height : 300,
-					redFrom : 90,
-					redTo : 100,
-					yellowFrom : 75,
-					yellowTo : 90,
-					minorTicks : 5
-				};
-
-				var chart1 = new google.visualization.Gauge(document.getElementById('chart1'));
-				var chart2 = new google.visualization.Gauge(document.getElementById('chart2'));
-				chart1.draw(data1, options);
-				chart2.draw(data2, options);
-			}
-		</script>
+	
+		<script type="text/javascript" src="./includes/smoothie.min.js"></script>
+		
 		<title></title>
 		<style type="text/css">
 			body {
@@ -67,80 +42,8 @@
 			<div class="visitors" id="numVisitors"></div>
 			<hr />
 			<br />
-			<table width="50%">
-				<tr>
-					<td width="25%"><span id='chart1' style="display: inline;"></span></td>
-					<td></td>
-					<td width="25%"><span id='chart2' style="display: inline;"></span></td>
-				</tr>
-
-			</table>
-			<br />
-			<script>
-				var t = 12971163, v = 70, data = d3.range(33).map(next);
-				// starting dataset
-
-				function next() {
-					return {
-						time : ++t,
-						value : v = ~~Math.max(10, Math.min(90, v + 10 * (Math.random() - .5)))
-					};
-				};
-
-				function redraw() {
-
-					var rect = chart.selectAll("rect").data(data, function(d) {
-						return d.time;
-					});
-
-					rect.enter().insert("rect", "line").attr("x", function(d, i) {
-						return x(i + 1) - .5;
-					}).attr("y", function(d) {
-						return h - y(d.value) - .5;
-					}).attr("width", w).attr("height", function(d) {
-						return y(d.value);
-					}).transition().duration(1000).attr("x", function(d, i) {
-						return x(i) - .5;
-					});
-
-					rect.transition().duration(1000).attr("x", function(d, i) {
-						return x(i) - .5;
-					});
-
-					rect.exit().transition().duration(1000).attr("x", function(d, i) {
-						return x(i - 1) - .5;
-					}).remove();
-
-				}
-
-				setInterval(function() {
-					data.shift();
-					data.push(next());
-					redraw();
-					d3.timer.flush();
-				}, 1100);
-
-				var w = 20, h = 80;
-				var x = d3.scale.linear().domain([0, 1]).range([0, w]);
-				var y = d3.scale.linear().domain([0, 100]).rangeRound([0, h]);
-
-				var chart = d3.select("body").append("svg").attr("class", "chart").attr("width", w * data.length - 1).attr("height", h);
-
-				chart.selectAll("rect").data(data).enter().append("rect").attr("x", function(d, i) {
-					return x(i) - .5;
-				}).attr("y", function(d) {
-					return h - y(d.value) - .5;
-				}).attr("width", w).attr("height", function(d) {
-					return y(d.value);
-				});
-
-				chart.append("line").attr("x1", 0).attr("x2", w * data.length).attr("y1", h - .5).attr("y2", h - .5).style("stroke", "#FFF");
-				chart.selectAll("text").data(data).enter().append("text").attr("x", x).attr("y", y).attr("dx", -3)// padding-right
-				.attr("dy", ".35em")// vertical-align: middle
-				.attr("text-anchor", "end")// text-align: right
-				.text(String);
-
-			</script>
+			
+			
 
 			<br />
 			<br />
@@ -168,9 +71,9 @@
 		var line2 = new TimeSeries();
 		
 		smoothie.addTimeSeries(line1, {
-			strokeStyle : 'rgb(0, 255, 0)',
-			fillStyle : 'rgba(0, 255, 0, 0.4)',
-			lineWidth : 3
+			strokeStyle : 'rgb(0, 255, 125)',
+			fillStyle : 'rgba(0, 255, 125, 0.4)',
+			lineWidth : 1
 		});
 		smoothie.addTimeSeries(line2, {
 			strokeStyle : 'rgb(255, 0, 255)',
